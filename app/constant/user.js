456ffbc83472306e820b7d1f9376572a05869016
Qi Zhang <zhangqi444@gmail.com>
2021-06-01 08:11:04 -0700

    添加一些文档

--- app/constant/user.js
+++ app/constant/user.js
@@ -1,7 +1,7 @@
 module.exports = {
   ONBOARDING_STATUS: {
-    ONBOARDED: 'ONBOARDED',
-    VERIFIED: 'VERIFIED',
+    ONBOARDED: 'ONBOARDED', // 用户完成注册，如邮箱或手机已经验证，如有必要，也可以添加EMAIL_ONBOARDED等来进一步区分
+    VERIFIED: 'VERIFIED', // 用户是实名、职业验证的用户，如有必要，也可以添加IDENTIFICATION_VERIFIED等来进一步区分
     EMAIL_VERIFIED: 'EMAIL_VERIFIED',
     DEFAULT: 'DEFAULT',
   },
