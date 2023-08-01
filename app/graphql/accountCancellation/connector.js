c9f4c9468358555cd3b6f4a2cd728fad4a5015bd
唐正Development <12324791+tang-zheng-development@user.noreply.gitee.com>
2023-08-01 12:27:31 +0800

    已完成所有接口的框架 升级README

---
+++ app/graphql/accountCancellation/connector.js
@@ -0,0 +1,17 @@
+// connector.js
+// This file simulates the connection to a backend or a database and returns mocked data.
+
+const accountData = {
+    12344224: {
+        status: "cancelled",
+        msg: "Account successfully cancelled.",
+    },
+};
+
+const getAccountCancellation = (number) => {
+    return accountData[number] || null;
+};
+
+module.exports = {
+    getAccountCancellation,
+};
---
+++ app/graphql/changeUser/connector.js
@@ -0,0 +1,12 @@
+
+const mockUserData = {
+    name: "TestUserName",
+    imageUrl: "https://example.com/profile.jpg",
+    sex: "test",
+    dsc: "blalablalablalablalablalablalablalablalablalablalablalablala",
+    email: "test@example.com",
+};
+
+module.exports = {
+    getUserData: () => mockUserData,
+};
