1ec8315cdbaaf75097289e0e13f9122449991aed
caohanzhong <342292451@qq.com>
2023-08-11 10:06:09 +0800

    修改报错

--- app/graphql/passwordLogin/resolver.js
+++ app/graphql/passwordLogin/resolver.js
@@ -2,9 +2,25 @@ const UserService = require('../../service/user');
 
 module.exports = {
     Mutation: {
-        passwordLogin: async (_, { input }, ctx) => {
-            const passwordLoginService = new UserService(ctx);
-            return passwordLoginService.passwordLogin(input);
+        passwordLogin(root, { mobile, area, password }, ctx) {
+            return ctx.connector.passwordLogin.passwordLogin(mobile, area, password);
+            // const passwordLoginService = new UserService(ctx);
+            // return passwordLoginService.passwordLogin(input);
+        },   
+        sendResetPasswordCode(root, { code }, ctx){
+            return ctx.connector.passwordLogin.sendResetPasswordCode(code)
+            // const SendResetPasswordCode = new UserService(ctx);
+            // return SendResetPasswordCode.sendResetPasswordCode(code);
+        },
+        verifyResetPasswordCode: async (_, { code }, ctx) => {
+            return ctx.connector.passwordLogin.verifyResetPasswordCode(code);
+            // const VerifyResetPasswordCode = new UserService(ctx);
+            // return VerifyResetPasswordCode.verifyResetPasswordCode(code);
+        },
+        resetPassword(_, { newPassword }, ctx) {
+            return ctx.connector.passwordLogin.resetPassword(newPassword);
+            // const resetPasswordService = new UserService(ctx);
+            // return resetPasswordService.resetPassword(newPassword);
         },
     }
 }
